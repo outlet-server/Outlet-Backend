b103f99580d3aa793c251b88c1aa5ca381cd6dfe
aznos <aznosreal@gmail.com>
2025-12-03 14:54:19 -0500

    Give status code

--- api/uptimeInfo.ts
+++ api/uptimeInfo.ts
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/online", (req, res) => {
-    res.json({ok: true})
+    res.status(200).json({ok: true});
 })
 
 module.exports = router;
